6fd1d1eff77df1db5bf7237abf3baaa26a03f604
Vanessa Bautista <vanessa@digit4l.co>
2022-10-26 15:24:14 -0500

    Segundo commit, autenticación header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EstudiantesService } from './services/estudiantes.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pruebaTecnicaFront';
+
+  constructor(private svc: EstudiantesService) {}
+
+  ngOnInit(): void {
+    this.login();
+  }
+
+  login() {
+    let data = {email: 'jbautistavallejo@gmail.com', password: '123456'};
+    this.svc.login(data).subscribe((res: any) => {
+      localStorage.setItem('token',res.access_token);      
+    });
+  }
 }
--- src/app/services/estudiantes.service.ts
+++ src/app/services/estudiantes.service.ts
@@ -1,39 +1,46 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstudiantesService {
+  token = localStorage.getItem('token');
+
+  headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.token);
 
   constructor(private _http: HttpClient) { }
 
+  login(data: any) {    
+    return this._http.post(environment.url + 'login', data);
+  }
+
   listarEstudiantes() {
-    return this._http.get(environment.url + 'listar');
+    return this._http.get(environment.url + 'listar', {headers: this.headers});
   }
 
   crearAlumno(data: any) {
-    return this._http.post(environment.url + 'crear', data);
+    return this._http.post(environment.url + 'crear', data, {headers: this.headers});
   }
 
   editarAlumno(id: any) {
-    return this._http.get(environment.url + 'editarAlumno/' + id);
+    return this._http.get(environment.url + 'editarAlumno/' + id, {headers: this.headers});
   }
 
   actualizarDatos(data: any) {
-    return this._http.post(environment.url + 'actualizar', data);
+    return this._http.post(environment.url + 'actualizar', data, {headers: this.headers});
   }
 
   cambiarEstado(data: any) {
-    return this._http.post(environment.url + 'estado', data);
+    return this._http.post(environment.url + 'estado', data, {headers: this.headers});
   }
 
   listarTipoDocumentos() {
-    return this._http.get(environment.url + 'listarTipoDocumento');
+    return this._http.get(environment.url + 'listarTipoDocumento', {headers: this.headers});
   }
 
   listarCursos() {
-    return this._http.get(environment.url + 'listarCursos');
+    return this._http.get(environment.url + 'listarCursos', {headers: this.headers});
   }
 }
